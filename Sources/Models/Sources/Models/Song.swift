744379222413cea2f46c7c3e30f9a5a5e246dcf7
kevink2023 <krk252@psu.edu>
2024-02-08 13:01:40 -0800

    Working on models

--- Sources/Models/Sources/Models/Song.swift
+++ Sources/Models/Sources/Models/Song.swift
@@ -8,29 +8,42 @@
 import Foundation
 
 public struct Song: Media {
-    public let id: UUID = UUID()
+    public let id: SongID
     public let source: MediaSource
     public let duration: TimeInterval
     
     public let title: String?
-    public let artist: Artist?
-    public let album: Album?
     public let art: MediaArt?
     
+    public let artist: ArtistID?
+    public let album: AlbumID?
+    
     public init(
-        source: MediaSource
+        id: SongID
+        , source: MediaSource
         , duration: TimeInterval
         , title: String? = nil
-        , artist: Artist? = nil
-        , album: Album? = nil
+        , artist: ArtistID? = nil
+        , album: AlbumID? = nil
         , art: MediaArt? = nil
     ) {
+        self.id = id
         self.source = source
         self.duration = duration
         self.title = title
         self.artist = artist
         self.album = album
         self.art = art
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case source
+        case duration
+        case title
+        case artist
+        case album
+        case art
     }
 }
 
