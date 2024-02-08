be5ffafd288444c582f0c6be8ee2347649d28adf
kevink2023 <krk252@psu.edu>
2024-02-07 18:26:35 -0800

    new start

---
+++ Boomic/BoomicApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoomicApp.swift
+//  Boomic
+//
+//  Created by Kevin Kelly on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BoomicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
