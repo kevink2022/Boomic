87b9ca5f7668b7691caacfbd45491f5d2177042b
kevink2022 <kevink2019@gmail.com>
2024-02-09 23:19:49 -0800

    model tests started

--- Sources/Models/Tests/ModelsTests/ModelsTests.swift
+++ Sources/Models/Tests/ModelsTests/ModelsTests.swift
@@ -2,11 +2,17 @@ import XCTest
 @testable import Models
 
 final class ModelsTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    let decoder = JSONDecoder()
+    
+    func test_decodeJSON_parsesKeyInformation() {
+        
+        guard let jsonData = Song.aCagedPersonaJSON.data(using: .utf8),
+              let song = try? decoder.decode([Song].self, from: jsonData).first else {
+            XCTFail("Failed to decode test data")
+            return
+        }
+        
+        XCTAssertEqual(song.id, UUID(uuidString: "1a2b3c4d-5e6f-7a8b-9c0d-1e2f3a4b5c6d"))
     }
 }
