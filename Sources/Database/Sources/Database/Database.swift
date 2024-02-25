edeaa46b3bfeff42688f86d250654138af8d8cbd
kevink2023 <krk252@psu.edu>
2024-02-25 13:50:42 -0800

    generalized db for future models, probably didnt help tho lol

--- Sources/Database/Sources/Database/Database.swift
+++ Sources/Database/Sources/Database/Database.swift
@@ -9,11 +9,98 @@ import Foundation
 import Models
 
 public protocol Database {
-    func getSongs(_ songIDs: [SongID]?) async throws -> [Song]
-    func getAlbums(_ albumIDs: [AlbumID]?) async throws -> [Album]
-    func getArtists(_ artistIDs: [ArtistID]?) async throws -> [Artist]
-    
-    func saveSongs(_ songsToSave: [Song]) async throws
-    func saveAlbums(_ albumsToSave: [Album]) async throws
-    func saveArtists(_ artistsToSave: [Artist]) async throws
+    func get<Getting: Model> (_ getting: Getting.Type) async throws -> [Getting]
+    func get<Getting: Model, From: Model> (_ getting: Getting.Type, from object: From) async throws -> [Getting]
+    func save<T: Model>(_ objects: [T]) async throws
 }
+
+public enum DatabaseError: LocalizedError, Equatable {
+    case dataCorrupted(URL)
+    case unresolvedTable(Any.Type)
+    case unresolvedRelation(Any.Type, Any.Type)
+    
+    public var errorDescription: String? {
+        switch self {
+        case .dataCorrupted(let url): return "Data cannot be read from \(url)"
+        case .unresolvedTable(let type): return "Type \"\(type)\" has no associated table."
+        case .unresolvedRelation(let from, let to): return "Type \"\(from)\" does not map to type \"\(to)\"."
+        }
+    }
+    
+    public static func == (lhs: DatabaseError, rhs: DatabaseError) -> Bool {
+        switch (lhs, rhs) {
+            case (.dataCorrupted(let lhsURL), .dataCorrupted(let rhsURL)):
+                return lhsURL == rhsURL
+            case (.unresolvedTable(let lhsType), .unresolvedTable(let rhsType)):
+                return String(describing: lhsType) == String(describing: rhsType)
+            case (.unresolvedRelation(let lhsFrom, let lhsTo), .unresolvedRelation(let rhsFrom, let rhsTo)):
+                return String(describing: lhsFrom) == String(describing: rhsFrom) && String(describing: lhsTo) == String(describing: rhsTo)
+            default:
+                return false
+            }
+    }
+}
+
+public protocol Model: Identifiable, Codable {
+    var id: UUID { get }
+}
+
+extension Song: Model {}
+extension Album: Model {}
+extension Artist: Model {}
+
+/// Will likely become db implementation specific. So much for saving boilerplate lol.
+
+internal protocol RelationalModel: Model {
+    func to<T:RelationalModel>(_ object: T) throws -> [UUID]
+}
+
+extension Song: RelationalModel {
+    
+    internal func to<T:RelationalModel>(_ object: T) throws -> [UUID] {
+        switch T.self {
+        
+        case is Album.Type:
+            guard let album = self.album else { return [UUID]() }
+            return [album]
+        
+        case is Artist.Type:
+            return self.artists
+        
+        default: throw DatabaseError.unresolvedRelation(Song.self, T.self)
+        }
+    }
+}
+
+extension Album: RelationalModel {
+    
+    internal func to<T:RelationalModel>(_ object: T) throws -> [UUID] {
+        switch T.self {
+        
+        case is Song.Type:
+            return self.songs
+        
+        case is Artist.Type: 
+            return self.artists
+        
+        default: throw DatabaseError.unresolvedRelation(Album.self, T.self)
+        }
+    }
+}
+
+extension Artist: RelationalModel {
+    
+    internal func to<T:RelationalModel>(_ object: T) throws -> [UUID] {
+        switch T.self {
+        
+        case is Song.Type:
+            return self.songs
+        
+        case is Album.Type:
+            return self.albums
+        
+        default: throw DatabaseError.unresolvedRelation(Artist.self, T.self)
+        }
+    }
+}
+
--- Sources/Database/Tests/DatabaseTests/JSONArrayDatabaseTests.swift
+++ Sources/Database/Tests/DatabaseTests/JSONArrayDatabaseTests.swift
@@ -3,19 +3,28 @@ import Models
 import ModelsMocks
 @testable import Database
 
+
+
 final class JSONArrayDatabaseTests: XCTestCase {
     
-    private static let dbDefaultURL_ios = URL.applicationSupportDirectory
-        .appending(component: "TestDatabase")
-    private static let songsDefaultURL_ios = URL.applicationSupportDirectory
-        .appending(component: "TestDatabase")
-        .appending(component: "songs.json")
-    private static let albumsDefaultURL_ios = URL.applicationSupportDirectory
-        .appending(component: "TestDatabase")
-        .appending(component: "albums.json")
-    private static let artistsDefaultURL_ios = URL.applicationSupportDirectory
-        .appending(component: "TestDatabase")
-        .appending(component: "artists.json")
+    private typealias C = TestConstants
+    private struct TestConstants {
+        private static let directoryString = "TestDatabase/"
+        
+        static let dbDefaultURL_ios = URL.applicationSupportDirectory
+            .appending(component: TestConstants.directoryString)
+        static let songsDefaultURL_ios = URL.applicationSupportDirectory
+            .appending(component: TestConstants.directoryString)
+            .appending(component: "songs.json")
+        static let albumsDefaultURL_ios = URL.applicationSupportDirectory
+            .appending(component: TestConstants.directoryString)
+            .appending(component: "albums.json")
+        static let artistsDefaultURL_ios = URL.applicationSupportDirectory
+            .appending(component: TestConstants.directoryString)
+            .appending(component: "artists.json")
+    }
+    
+   
     
     private let fileManager = FileManager()
     
@@ -24,61 +33,61 @@ final class JSONArrayDatabaseTests: XCTestCase {
             decoder: JSONDecoder()
             , encoder: JSONEncoder()
             , fileManager: FileManager()
-            , songsURL: JSONArrayDatabaseTests.songsDefaultURL_ios
-            , albumsURL: JSONArrayDatabaseTests.albumsDefaultURL_ios
-            , artistsURL: JSONArrayDatabaseTests.artistsDefaultURL_ios
+            , songsURL: C.songsDefaultURL_ios
+            , albumsURL: C.albumsDefaultURL_ios
+            , artistsURL: C.artistsDefaultURL_ios
         )
     }
     
     private func deleteTestDB() throws {
-        if fileManager.fileExists(atPath: JSONArrayDatabaseTests.dbDefaultURL_ios.path()) {
-            try fileManager.removeItem(at: JSONArrayDatabaseTests.dbDefaultURL_ios)
+        let urls = try fileManager.contentsOfDirectory(at: URL.applicationSupportDirectory, includingPropertiesForKeys: nil)
+        
+        if urls.contains(C.dbDefaultURL_ios) {
+            try fileManager.removeItem(at: C.dbDefaultURL_ios)
         }
     }
     
     private func createEmptyTestDB() throws {
         try deleteTestDB()
-        //try fileManager.createDirectory(at: JSONArrayDatabaseTests.dbDefaultURL_ios, withIntermediateDirectories: false)
-        fileManager.createFile(atPath: JSONArrayDatabaseTests.songsDefaultURL_ios.path(), contents: nil)
-        fileManager.createFile(atPath: JSONArrayDatabaseTests.albumsDefaultURL_ios.path(), contents: nil)
-        fileManager.createFile(atPath: JSONArrayDatabaseTests.artistsDefaultURL_ios.path(), contents: nil)
+        try fileManager.createDirectory(at: C.dbDefaultURL_ios, withIntermediateDirectories: false)
         
-        print("songs at \(JSONArrayDatabaseTests.songsDefaultURL_ios.path()): \(fileManager.fileExists(atPath: JSONArrayDatabaseTests.songsDefaultURL_ios.path()))")
-        print("albums at \(JSONArrayDatabaseTests.albumsDefaultURL_ios.path()): \(fileManager.fileExists(atPath: JSONArrayDatabaseTests.albumsDefaultURL_ios.path()))")
-        print("artists at \(JSONArrayDatabaseTests.artistsDefaultURL_ios.path()): \(fileManager.fileExists(atPath: JSONArrayDatabaseTests.artistsDefaultURL_ios.path()))")
+        let emptyData = "[]".data(using: .utf8)
+        try emptyData!.write(to: C.songsDefaultURL_ios)
+        try emptyData!.write(to: C.albumsDefaultURL_ios)
+        try emptyData!.write(to: C.artistsDefaultURL_ios)
     }
     
     private func createValidTestDB() throws {
         try deleteTestDB()
-        //try fileManager.createDirectory(at: JSONArrayDatabaseTests.dbDefaultURL_ios, withIntermediateDirectories: false)
+        try fileManager.createDirectory(at: C.dbDefaultURL_ios, withIntermediateDirectories: false)
+        
         let songsData = Song.songsJSON.data(using: .utf8)
         let albumsData = Album.albumsJSON.data(using: .utf8)
         let artistsData = Artist.artistsJSON.data(using: .utf8)
-        
-        try songsData!.write(to: JSONArrayDatabaseTests.songsDefaultURL_ios)
-        try albumsData!.write(to: JSONArrayDatabaseTests.albumsDefaultURL_ios)
-        try artistsData!.write(to: JSONArrayDatabaseTests.artistsDefaultURL_ios)
-        
-        print("songs at \(JSONArrayDatabaseTests.songsDefaultURL_ios.path()): \(fileManager.fileExists(atPath: JSONArrayDatabaseTests.songsDefaultURL_ios.path()))")
-        print("albums at \(JSONArrayDatabaseTests.albumsDefaultURL_ios.path()): \(fileManager.fileExists(atPath: JSONArrayDatabaseTests.albumsDefaultURL_ios.path()))")
-        print("artists at \(JSONArrayDatabaseTests.artistsDefaultURL_ios.path()): \(fileManager.fileExists(atPath: JSONArrayDatabaseTests.artistsDefaultURL_ios.path()))")
+        try songsData!.write(to: C.songsDefaultURL_ios)
+        try albumsData!.write(to: C.albumsDefaultURL_ios)
+        try artistsData!.write(to: C.artistsDefaultURL_ios)
     }
     
-    private func createInvalidTestDB() throws {
+    private func createCorruptTestDB() throws {
         try deleteTestDB()
-        //try fileManager.createDirectory(at: JSONArrayDatabaseTests.dbDefaultURL_ios, withIntermediateDirectories: false)
-        fileManager.createFile(atPath: JSONArrayDatabaseTests.songsDefaultURL_ios.path(), contents: nil)
-        fileManager.createFile(atPath: JSONArrayDatabaseTests.albumsDefaultURL_ios.path(), contents: nil)
-        fileManager.createFile(atPath: JSONArrayDatabaseTests.artistsDefaultURL_ios.path(), contents: nil)
+        try fileManager.createDirectory(at: C.dbDefaultURL_ios, withIntermediateDirectories: false)
+        
+        let emptyData = "[zoinks, scoob]".data(using: .utf8)
+        try emptyData!.write(to: C.songsDefaultURL_ios)
+        try emptyData!.write(to: C.albumsDefaultURL_ios)
+        try emptyData!.write(to: C.artistsDefaultURL_ios)
     }
     
     // MARK: DB Persistant State Tests
     
     func test_initNoDB() async {
+        defer { try? deleteTestDB() }
+        
         do {
             try deleteTestDB()
         } catch {
-            XCTFail("Failed test set up: \(error.localizedDescription)")
+            XCTFail("Failed to set up test: \(error.localizedDescription)")
         }
         
         let sut: JSONArrayDatabase
@@ -89,19 +98,21 @@ final class JSONArrayDatabaseTests: XCTestCase {
             return
         }
         
-        let songs = try? await sut.getSongs()
+        let songs = try? await sut.get(Song.self)
         XCTAssertEqual(songs!.count, 0)
-        let albums = try? await sut.getAlbums()
+        let albums = try? await sut.get(Album.self)
         XCTAssertEqual(albums!.count, 0)
-        let artists = try? await sut.getArtists()
+        let artists = try? await sut.get(Artist.self)
         XCTAssertEqual(artists!.count, 0)
     }
     
     func test_initEmptyDB() async {
+        defer { try? deleteTestDB() }
+        
         do {
             try createEmptyTestDB()
         } catch {
-            XCTFail("Failed test set up: \(error.localizedDescription)")
+            XCTFail("Failed to set up test: \(error.localizedDescription)")
         }
         
         let sut: JSONArrayDatabase
@@ -112,19 +123,21 @@ final class JSONArrayDatabaseTests: XCTestCase {
             return
         }
         
-        let songs = try? await sut.getSongs()
+        let songs = try? await sut.get(Song.self)
         XCTAssertEqual(songs!.count, 0)
-        let albums = try? await sut.getAlbums()
+        let albums = try? await sut.get(Album.self)
         XCTAssertEqual(albums!.count, 0)
-        let artists = try? await sut.getArtists()
+        let artists = try? await sut.get(Artist.self)
         XCTAssertEqual(artists!.count, 0)
     }
     
     func test_initValidDB() async {
+        defer { try? deleteTestDB() }
+        
         do {
             try createValidTestDB()
         } catch {
-            XCTFail("Failed test set up: \(error.localizedDescription)")
+            XCTFail("Failed to set up test: \(error.localizedDescription)")
         }
         
         let sut: JSONArrayDatabase
@@ -135,16 +148,29 @@ final class JSONArrayDatabaseTests: XCTestCase {
             return
         }
         
-        let songs = try? await sut.getSongs()
+        let songs = try? await sut.get(Song.self)
         XCTAssertEqual(songs!.count, 21)
-        let albums = try? await sut.getAlbums()
+        let albums = try? await sut.get(Album.self)
         XCTAssertEqual(albums!.count, 2)
-        let artists = try? await sut.getArtists()
+        let artists = try? await sut.get(Artist.self)
         XCTAssertEqual(artists!.count, 7)
     }
     
     func test_initCorruptDB() async {
+        defer { try? deleteTestDB() }
         
+        do {
+            try createCorruptTestDB()
+        } catch {
+            XCTFail("Failed to set up test: \(error.localizedDescription)")
+        }
+        
+        do {
+            _ = try initTestDB()
+            XCTFail("Database initialized with corrupt data.")
+        } catch {
+            XCTAssertEqual(error as! DatabaseError, DatabaseError.dataCorrupted(C.songsDefaultURL_ios))
+        }
     }
     
     // MARK: DB Protocol Tests
@@ -157,9 +183,39 @@ final class JSONArrayDatabaseTests: XCTestCase {
 //        
 //    }
 //    
-//    func test_getValid() async {
-//        
-//    }
+    func test_getValid() async {
+        defer { try? deleteTestDB() }
+        
+        do {
+            try createValidTestDB()
+        } catch {
+            XCTFail("Failed to set up test: \(error.localizedDescription)")
+        }
+        
+        let sut: JSONArrayDatabase
+        do {
+            sut = try initTestDB()
+        } catch {
+            XCTFail("Failed to init Database: \(error.localizedDescription)")
+            return
+        }
+        
+        let albums = try! await sut.get(Album.self)
+        
+        let album = albums.first!
+        let artists: [Artist]
+        let songs: [Song]
+        do {
+            artists = try await sut.get(Artist.self, from: album)
+            songs = try await sut.get(Song.self, from: album)
+        } catch {
+            XCTFail("Failed to get: \(error.localizedDescription)")
+            return
+        }
+        
+        XCTAssertEqual(artists.count, 5)
+        XCTAssertEqual(songs.count, 10)
+    }
 //    
 //    func test_saveValid() async {
 //        
