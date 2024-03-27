9bd4a7081fea2fe96720900bfd4f44d36c72802e
kevink2023 <krk252@psu.edu>
2024-03-26 19:55:25 -0700

    easy, minimum viable, untested tag parsers

---
+++ Sources/MediaFileKit/Sources/MediaFileKit/AudioToolboxParser.swift
@@ -0,0 +1,102 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kevin Kelly on 3/22/24.
+//
+
+import Foundation
+import AudioToolbox
+
+public final class AudioToolboxParser {
+    
+    public let source: URL
+    public let hasTags: Bool
+    public let hasEmbeddedArt: Bool
+    private let tags: [String : Any]?
+    private typealias Static = AudioToolboxParser
+    
+    init(file: URL) {
+        source = file
+        
+        guard let fileID = Static.openFile(url: file) else {
+            hasTags = false
+            hasEmbeddedArt = false
+            tags = nil
+            return
+        }
+        defer { Static.closeFile(fileID) }
+        
+        tags = Static.initTags(fileID)
+        hasTags = tags == nil ? false : true
+        hasEmbeddedArt = Static.hasEmbedded(fileID)
+    }
+    
+    private static func openFile(url: URL) -> AudioFileID? {
+        guard let fileType = Static.fileToType(url) else { return nil }
+        
+        var fileID: AudioFileID? = nil
+        let fileOpenStatus: OSStatus = AudioFileOpenURL(url as CFURL, .readPermission, fileType, &fileID)
+        guard fileOpenStatus == noErr else { return nil }
+        return fileID
+    }
+    
+    private static func closeFile(_ fileID: AudioFileID) {
+        AudioFileClose(fileID)
+    }
+    
+    private static func initTags(_ fileID: AudioFileID) -> [String : Any]? {
+        var dict: CFDictionary? = nil
+        var dataSize = UInt32(MemoryLayout<CFDictionary?>.size(ofValue: dict))
+        
+        let getPropertyStatus: OSStatus = AudioFileGetProperty(fileID, kAudioFilePropertyInfoDictionary, &dataSize, &dict)
+        guard getPropertyStatus == noErr else { return nil }
+        
+        guard let cfDict = dict else { return nil }
+        let tagsDict = NSDictionary.init(dictionary: cfDict)
+        return tagsDict as? [String : Any]
+    }
+    
+    private static func hasEmbedded(_ fileID: AudioFileID) -> Bool {
+        var dataSize: UInt32 = 0
+        let status = AudioFileGetPropertyInfo(fileID, kAudioFilePropertyAlbumArtwork, &dataSize, nil)
+
+        guard status == noErr && dataSize > 0 else { return false }
+            
+        var artworkData: UnsafeMutableRawPointer? = nil
+        let result = AudioFileGetProperty(fileID, kAudioFilePropertyAlbumArtwork, &dataSize, &artworkData)
+        
+        guard result == noErr && artworkData != nil else { return false }
+            
+        return true
+    }
+    
+    private static func fileToType(_ file: URL) -> AudioFileTypeID? {
+        switch file.pathExtension.lowercased() {
+        case "mp3": return kAudioFileMP3Type
+        case "flac": return kAudioFileFLACType
+        case "m4a": return kAudioFileM4AType
+        default: return nil
+        }
+    }
+    
+    private func parseTrackNumber(_ trackNumberString: String?) -> Int? {
+        guard let trackNumberString = trackNumberString else { return nil }
+        return Int(trackNumberString.split(separator: "/")[0])
+    }
+    
+    public var allTags: [String : Any]? { tags }
+    
+    public var title: String? { tags?["title"] as? String }
+    public var artist: String? { tags?["artist"] as? String }
+    public var album: String? { tags?["album"] as? String }
+    public var genre: String? { tags?["genre"] as? String }
+    
+    public var year: Int? { tags?["year"] as? Int ?? tags?["recorded date"] as? Int }
+    public var date: Date? { tags?["recorded date"] as? Date }
+    
+    public var duration: TimeInterval? { TimeInterval(tags?["approximate duration in seconds"] as? String ?? "") }
+    public var trackNumber: Int? { parseTrackNumber(tags?["track number"] as? String) }
+
+    public var comments: String? { tags?["comments"] as? String }
+}
---
+++ Sources/MediaFileKit/Sources/MediaFileKit/MediaFileKit.swift
@@ -0,0 +1,90 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+import Models
+
+public protocol MediaFileInterface {
+    var libraryDirectory: URL { get }
+    
+    func newSongs(existing songs: [Song]) async throws -> [Song]
+}
+
+public enum MediaFileInterfaceError: LocalizedError, Equatable {
+    case enumeratorInitFail(URL)
+    
+    public var errorDescription: String? {
+        switch self {
+        case .enumeratorInitFail(let url): return "An enumerator could not be initialized at the library URL: \(url)"
+        }
+    }
+    
+    public static func == (lhs: MediaFileInterfaceError, rhs: MediaFileInterfaceError) -> Bool {
+        switch (lhs, rhs) {
+        case (.enumeratorInitFail(let lhs), .enumeratorInitFail(let rhs)):
+            return String(describing: lhs) == String(describing: rhs)
+        }
+    }
+}
+
+public final class LocalMediaFileInterface: MediaFileInterface {
+    
+    public let libraryDirectory: URL
+    private let fileManager: FileManager
+    
+    public init(
+        libraryDirectory: URL = URL.documentsDirectory
+        , fileManager: FileManager = FileManager()
+    ) {
+        self.libraryDirectory = libraryDirectory
+        self.fileManager = fileManager
+    }
+    
+    public func newSongs(existing songs: [Song]) async throws -> [Song] {
+        
+        let existingFiles = songs.compactMap { song in
+            if case .local(let url) = song.source {
+                return url
+            }
+            return nil
+        }
+        
+        let newFiles = try newFiles(at: libraryDirectory, known: existingFiles)
+
+        let newSongs = newFiles.map { file in
+            let internalParser = AudioToolboxParser(file: file)
+            let externalParser = DirectoryParser(file: file)
+            
+            let albumArt: MediaArt? = {
+                if internalParser.hasEmbeddedArt { return .embedded }
+                if let art = externalParser.albumArt { return .local(art) }
+                return nil
+            }()
+                        
+            return Song(
+                id: UUID()
+                , source: .local(url: file)
+                , duration: internalParser.duration ?? 0
+                , title: internalParser.title
+                , trackNumber: internalParser.trackNumber
+                , discNumber: externalParser.discNumber
+                , art: albumArt
+                , artistName: internalParser.artist
+                , albumTitle: internalParser.album
+            )
+        }
+
+        return newSongs
+    }
+    
+    private func newFiles(at url: URL, known files: [URL]) throws -> [URL] {
+        
+        guard let enumerator = fileManager.enumerator(at: url, includingPropertiesForKeys: nil) else { throw MediaFileInterfaceError.enumeratorInitFail(url) }
+        
+        return enumerator.allObjects
+            .compactMap { $0 as? URL }
+            .filter { !files.contains($0) }
+    }
+    
+}
+
